87520982debe515c656cc95cf1f652c758c47a01
HashirIsmail <84286266+HashirIsmail@users.noreply.github.com>
2021-05-16 23:01:04 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+//This command is used to print on
+//Google
+console.log("Welcome to js")
+//variable
+
+//var name = 'Hashir';
+//console.log(name);
+
+//var age = 13;
+//console.log(age);
+
+var firstname = 'Hashir';
+var lastname = 'Siddiqui';
+var space = ' '; //for creating space
+
+console.log(firstname + space + lastname) //for professional
+console.log(firstname +' '+ lastname + space + 'My age is =' + age) //for beginners
+
+var name = prompt("🏷️What is your name?")
+document.getElementById('myname').innerHTML=name;
+
+var age = prompt("🙂What is your age?")
+document.getElementById('myage').innerHTML=age;
+
+var grade = prompt("🎓In which Grade do u study?")
+document.getElementById('mygrade').innerHTML=grade;
+
+var food = prompt("🧆Which food do u like the most?")
+document.getElementById('myffood').innerHTML=food;
+
+var hobbie = prompt("🤿What is your hobbie?")
+document.getElementById('myhobbie').innerHTML=hobbie;
+
+var country = prompt("🏁In which country do you live?")
+document.getElementById('country').innerHTML=country;
+alert('Thanks for your Information😃');
